04a9c3a71e05698524a4fbf22923799ce4cb7799
Rick <sernaquericardo10@gmail.com>
2026-04-27 17:19:06 -0500

    cambios de el sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from './components/panellateral/sidebar.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent],
+  imports: [RouterOutlet, SidebarComponent,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'StreetviewGM';
+  collapsed = false;
 }
