8eee7dc423becbd0f5c746b46875196a307854e5
Rick <sernaquericardo10@gmail.com>
2026-04-29 15:03:21 -0500

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,7 @@ import { MatrizGeneradaData } from './components/panellateral/sidebar.component'
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, MapaComponent, CommonModule],
+  imports: [SidebarComponent, MapaComponent, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
